2bb2567cc36b46af9d06c145269feb240bffa69c
kdchurch1994 <kdchurch@bsu.edu>
2021-09-04 18:01:11 -0400

    created the Comment.js model; updated the index.js file, added an updataed comments on the Comment.js and Post.js models

--- models/Comment.js
+++ models/Comment.js
@@ -1,28 +1,34 @@
 const { Model, DataTypes } = require('sequelize'); //Allows us to create the sequelize models
 const sequelize = require('../config/connection'); //Imports the connection to the database through sequelize
 
-class Post extends Model {} //Creates the Post model
+class Comment extends Model {} //Creates the Comment model
 
-Post.init( //Configure the fields of the SQL Table post
+Comment.init( //Configure the fields of the SQL Table comment
     {
         id: { //Creates and defines the id column
             type: DataTypes.INTEGER, //Stores the data type as an integer
             allowNull: false, //Does not allow null values
             primaryKey: true, //sets ID as the primary key
-            autoIncrement: true //autoIncrements the ID so that whenver a new post is created, the Post's ID is one value higher than the previous person that was added to the database
+            autoIncrement: true //autoIncrements the ID so that whenver a new comment is created, the new comment's ID is one value higher than the previous person that was added to the database
         },
-        title: { //Creates and defines the title column
-            type: DataTypes.STRING, //Stores the data type as a string
-            allowNull: false //Does not allow null values
-        },
-        content: { //creates and defines the content column
+        comment_text: { //creates and defines the comment_text column
             type: DataTypes.TEXT, //Stores the data type as Text, which is similar to string but allows for more characters
-            allowNull: false //Does not allow null values
+            allowNull: false, //Does not allow null values
+            validate: { //The comment must be more than one character in length
+                len: [1]
+            }
         },
         user_id: { //creates and defines the user_id column
             type: DataTypes.INTEGER, //Stores the data type as an integer
             references: { //references the user table and sets user_id as a foreign key using the id primary key from the user table.
                 model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: { //creates and defines the post_id column
+            type: DataTypes.INTEGER, //Stores the data type as an integer
+            references: { //references the post table and sets post_id as a foreign key using the id primary key from the post table.
+                model: 'post',
                 key: 'id'
             }
         }
@@ -31,8 +37,10 @@ Post.init( //Configure the fields of the SQL Table post
         sequelize, //passes in the imported sequelize connection, which is the connection to the tech_blog_db database
         freezeTableName: true, //doesn't allow the pluralize the name of the database table (stops the database from having multiple tables named post)
         underscored: true, //use underscores as opposed to camel-casing
-        modelName: 'post' //The name of our model (table) in the database
+        modelName: 'comment' //The name of our model (table) in the database
+
     }
+
 );
 
-module.exports = Post; //exports the post model
+module.exports = Comment; //exports the comment model
--- models/index.js
+++ models/index.js
@@ -1,13 +1,35 @@
 const User = require('./User'); //Imports the use of the User model
 const Post = require('./Post'); //Imports the use of the Post model
+const Comment = require('./Comment');
 
 User.hasMany(Post, { //A user can have many posts; Creates the relationship between the Post and User models using the user_id foreign key 
     foreignKey: 'user_id'
 });
+
 Post.belongsTo(User, { //Defines the relationship of the Post model to the user model
     foreignKey: 'user_id', //Imposes the constraint that a post can belong to one user, but not to many users
     onDelete: "cascade" //The link was created using the user_id foreign key
 }); //Automatically deletes the child table when a row in the parent table is deleted. 
 //In this case, when a user is deleted, the posts associated with their user_id are deleted
 
-module.exports = { User, Post }; //exports the use of the Post and User models by exporting the relationship created by index.js
+Comment.belongsTo(User, { //Defines the relationship of the Comment model to the User model
+    foreignKey: 'user_id', //Imposes the constraint that a comment can belong to one user, but not to many users
+    onDelete: "cascade" //The link was created using the user_id foreign key
+}); //Automatically deletes the child table when a row in the parent table is deleted. 
+//In this case, when a user is deleted, the comments associated with their user_id are deleted
+
+Comment.belongsTo(Post, { //Defines the relationship of the Comment model to the Post model
+    foreignKey: 'post_id', //Imposes the constraint that a comment can belong to one Post, but not to many Posts
+    onDelete: "cascade" //The link was created using the post_id foreign key
+}); //Automatically deletes the child table when a row in the parent table is deleted. 
+//In this case, when a post is deleted, the comments associated with their post_id are deleted
+
+User.hasMany(Comment, { //A user can have many comments; Creates the relationship between the Comment and User models using the user_id foreign key 
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, { //A post can have many comments; Creates the relationship between the Post and Comment models using the post_id foreign key 
+    foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment }; //exports the use of the Post, User, and Comment models by exporting the relationship created by index.js
