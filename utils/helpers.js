5e4e4232feb1accbb237ef003bec13961a5e950f
kdchurch1994 <kdchurch@bsu.edu>
2021-09-07 15:00:32 -0400

    added utilities to include withAuth for routes and to include a formated date for comments and posts; added paritials for this functionality and updated file paths

---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+const { DataTypes } = require("sequelize/types")
+
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new DataTypes(date).getData()}/${new Date(date).getFullYear()}`;
+    },
+}
