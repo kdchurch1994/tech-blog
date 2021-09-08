c89792fdbe26530972de9dd11c872c88067b78c0
kdchurch1994 <kdchurch@bsu.edu>
2021-09-08 00:01:56 -0400

    added a homepage.handlebars and home-routes to establish a homepage for the website; updated index.js to include the homeRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
