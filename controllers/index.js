23a1f3a853a0aed6935567b186ac33843eb10424
kdchurch1994 <kdchurch@bsu.edu>
2021-09-08 01:21:41 -0400

    added a dashboard-route file to include routes to the Post dashboard, a dashboard.handlebars file was also created

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api-routes');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
