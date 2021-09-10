bc3b0fa4124216617c00bb358e182e9e11b082dd
kdchurch1994 <kdchurch@bsu.edu>
2021-09-10 16:23:19 -0400

    updated create-post.js and create-post.handlebars

--- public/javascript/create-post.js
+++ public/javascript/create-post.js
@@ -1,8 +1,8 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('#post-title').value;
-    const content = document.querySelector('#content').value;
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#content').value.trim();
 
     const response = await fetch('/api/posts', {
         method: 'POST',
