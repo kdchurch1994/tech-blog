4e3a99d9d1be038e9f8b5288f7260183761d9ef0
kdchurch1994 <kdchurch@bsu.edu>
2021-09-10 01:38:03 -0400

    fixded error with signup.js

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,11 +1,11 @@
 async function signupformHandler(event) {
     event.preventDefault();
 
-    const emailAddress = document.querySelector('#email-signup').value.trim();
+    const email_address = document.querySelector('#email-signup').value.trim();
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && emailAddress & password) {
+    if (username && email_address & password) {
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({
